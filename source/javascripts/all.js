bf5413f558f485f4e3e0c42f21c0bc94dd49c24d
Shun Tamura <tamrin.bitterchococo@gmail.com>
2015-03-11 18:31:15 +0900

    all.jsへの追記を削除

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,15 +1,3 @@
 //= require "jquery/dist/jquery.min.js"
 //= require "bootstrap-sass/assets/javascripts/bootstrap.min.js"
 //= require_tree .
-
-function setHeader(){
-	// onload, onresize時にヘッダサイズを変更
-	$(window).on('load resize', function() {
-		// 画面の表示領域を取得
-		var wW = $(window).innerWidth();
-		var wH = $(window).innerHeight();
-		// cssに反映
-		$('header.jumbotron').css('height', wW+'px');
-		$('header.jumbotron').css('height', wH+'px');
-	});
-};
