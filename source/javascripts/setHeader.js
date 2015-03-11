4a643319adcb1bf88c94822f9fb8a299f5be6d88
Shun Tamura <tamrin.bitterchococo@gmail.com>
2015-03-11 23:09:00 +0900

    インデントを修正

--- source/javascripts/setHeader.js
+++ source/javascripts/setHeader.js
@@ -1,13 +1,12 @@
 // ヘッダのサイズ設定をする
 function setHeader(){
-	// onload, onresize時にヘッダサイズを変更
-	$(window).on('load resize', function() {
-		// 画面の表示領域を取得
-		var wW = $(window).innerWidth();
-		var wH = $(window).innerHeight();
-		// cssに反映
-		$('header.jumbotron').css('height', wW+'px');
-		$('header.jumbotron').css('height', wH+'px');
-	});
+ 	// onload, onresize時にヘッダサイズを変更
+ 	$(window).on('load resize', function() {
+ 		// 画面の表示領域を取得
+ 		var wW = $(window).innerWidth();
+ 		var wH = $(window).innerHeight();
+ 		// cssに反映
+ 		$('header.jumbotron').css('height', wW+'px');
+ 		$('header.jumbotron').css('height', wH+'px');
+ 	});
 };
-
