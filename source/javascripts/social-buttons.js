f771919d23ab97a57baba5b50d480e422d554b6c
Shun Tamura <tamrin.bitterchococo@gmail.com>
2015-03-13 03:22:56 +0900

    ツイートボタン、いいねボタンの追加 #13
    CSSでデザイン
    
    Pocket, はてブのボタンを設置
    
    Pocketボタンの横幅を調整
    
    ':after'削除
    
    'clearfix'追加
    
    はてブのタイトルはとりあえず放置, ポケットのCSSを調整

---
+++ source/javascripts/social-buttons.js
@@ -0,0 +1,11 @@
+// ソーシャルボタン用js読み込み
+(function(w,d){
+  w.___gcfg = {lang:"ja"};
+  var s,e = d.getElementsByTagName("script")[0],
+  a = function(u,f){if(!d.getElementById(f)){s = d.createElement("script");
+  s.src = u;if(f){s.id = f;}e.parentNode.insertBefore(s,e);}};
+  a("//platform.twitter.com/widgets.js","twitter-wjs");
+  a("//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0","facebook-jssdk");
+  a("https://widgets.getpocket.com/v1/j/btn.js?v=1");
+  a("//b.st-hatena.com/js/bookmark_button.js");
+})(this, document);
