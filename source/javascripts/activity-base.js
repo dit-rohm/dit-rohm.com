5304bfd477c6a089f1fdeae2574b97a2dc3eea24
Keita Nakanishi <nakanishy@yahoo.com>
2015-03-18 21:39:06 +0900

    デザイン周りの調整

--- source/javascripts/activity-base.js
+++ source/javascripts/activity-base.js
@@ -1,23 +1,13 @@
-//activityBase.js
 $(function () {
-
-  // onload, onresize時にmapサイズを変更
-  $(window).on('load resize', function() {
-    // 画面の表示領域を取得
-    var wW = $(window).innerWidth();
-    // cssに反映
-    $('#map').css('width', wW+'px');
-  });
-
   //map表示
   var latlng = new google.maps.LatLng(34.802876, 135.771218);
-    var options = {
-        zoom: 17,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        streetViewControl: false,
-        scrollwheel: false
-    };
+  var options = {
+      zoom: 17,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: false,
+      scrollwheel: false
+  };
   var map = new google.maps.Map(document.getElementById('map'), options);
 
   //マーカーの表示
