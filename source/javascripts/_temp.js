fd865f69dc896d1c59bbe4d3ca6b8202580efd59
Keita Nakanishi <nakanishy@yahoo.com>
2015-05-16 17:12:32 +0900

    不要なファイル削除とファイルの整理

---
+++ source/javascripts/_temp.js
@@ -0,0 +1,72 @@
+$(function() {
+  if ($(window).innerWidth() > 768) {
+    $('.jumbotron').addClass('fading');
+    var $header = $('.fading');
+    $(window).on('scroll', function() {
+      var scrollTop = $(this).scrollTop();
+      var height = $header.outerHeight();
+      if (height > scrollTop) {
+        var opacity = (height - scrollTop) / height;
+        $header.css({ 'opacity': opacity });
+      }
+    });
+  }
+
+  $('.carousel').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          autoplay: true,
+          autoplaySpeed: 2000,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // map表示
+  var latlng = new google.maps.LatLng(34.802760, 135.771273);
+  var options = {
+      zoom: 15,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false
+  };
+  var map = new google.maps.Map(document.getElementById('map'), options);
+  var styles =
+    [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
+  map.setOptions({styles: styles});
+
+  // マーカーの表示
+  var marker = new google.maps.Marker({
+    position: map.getCenter(),
+    title: 'ローム記念館',
+    map: map
+  });
+});
